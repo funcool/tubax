43af1910c26018381ced15549d3141207a4eb0cc
alonso.torres <alonso.torres@kaleidos.net>
2015-04-21 21:29:25 +0200

    Refactor

--- src/cljs_xml/core.cljs
+++ src/cljs_xml/core.cljs
@@ -1,13 +1,28 @@
 (ns cljs-xml.core
-  ;(:require-macros [cljs.core.async.macros :refer [go]])
   (:require [cljs.core.async :as async :refer [put! chan]]
             [js.sax :as sax]
             [clojure.zip :as zip]
             [clojure.string :as str]))
 
-(def tag-idx 0)
-(def att-idx 1)
-(def val-idx 2)
+(defn- add-node-document [node document]
+  (let [keytag (keyword (.-name node))
+        att-map (js->clj (.-attributes node) :keywordize-keys true)
+        node-value [keytag att-map nil]]
+    (-> document
+        (zip/append-child (vector keytag att-map []))
+        zip/down
+        zip/rightmost
+        zip/down
+        zip/rightmost)))
+
+(defn- close-node-document [node document]
+  (-> document zip/up zip/up))
+
+(defn- add-text [text document]
+  (let [trimmed (str/trim text)]
+    (if (not (empty? trimmed))
+      (zip/append-child document trimmed)
+      document)))
 
 (defn xml->clj [source]
   (let [ret-chan (chan)
@@ -15,39 +30,22 @@
         document (atom (zip/vector-zip []))]
     ;; OPEN TAG
     (set! (.-onopentag parser)
-          (fn [node]
-            ;(println (str "Open >> " (.-name node)) )
-            (let [keytag (keyword (.-name node))
-                  att-map (js->clj (.-attributes node) :keywordize-keys true)
-                  node-value [keytag att-map nil]]
-              (swap! document
-                     #(-> %
-                          (zip/append-child (vector keytag att-map []))
-                          zip/down
-                          zip/rightmost
-                          zip/down
-                          zip/rightmost)))))
+          #(swap! document (partial add-node-document %)))
 
     ;; CLOSE TAG
     (set! (.-onclosetag parser)
-          (fn [node]
-            (swap! document #(-> % zip/up zip/up))))
+          #(swap! document (partial close-node-document %)))
 
     ;; GET TEXT
     (set! (.-ontext parser)
-          (fn [text]
-            (let [trimmed (str/trim text)]
-              (when (not (empty? trimmed))
-                (swap! document zip/append-child trimmed)))))
+          #(swap! document (partial add-text %)))
 
     ;; END PARSING
     (set! (.-onend parser)
-          (fn []
-            (put! ret-chan (first (zip/root @document)))))
+          #(put! ret-chan (first (zip/root @document))))
 
     (set! (.-onerror parser)
-          (fn [error]
-            (put! ret-chan {:error (str error)})))
+          #(put! ret-chan {:error (str error)}))
 
     (.write parser source)
     (.close parser)
