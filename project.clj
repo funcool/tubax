853d204dc5c4c1e734f365a30100dc0f5620dd6e
alonso.torres <alonso.torres@kaleidos.net>
2015-04-24 20:15:15 +0200

    Improved project.clj

--- project.clj
+++ project.clj
@@ -4,14 +4,12 @@
   :license {:name "Apache 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.txt"}
 
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-3196"]
-                 [funcool/cljs-testrunners "0.1.0-SNAPSHOT"]]
-
-  :plugins [[lein-cljsbuild "1.0.5"]
-            [lein-externs "0.1.3"]]
-
-  :hooks [leiningen.cljsbuild]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0"]
+                                  [org.clojure/clojurescript "0.0-3196"]
+                                  [funcool/cljs-testrunners "0.1.0-SNAPSHOT"]]
+                   :plugins [[lein-cljsbuild "1.0.5"]
+                             [lein-externs "0.1.3"]]
+                   :hooks [leiningen.cljsbuild]}}
 
   :test-paths ["test"]
   :source-paths ["src" "test"]
