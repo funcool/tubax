42492d2c19fb60be1733ca17271742377b81a9ce
alonso.torres <alonso.torres@kaleidos.net>
2015-04-25 21:08:16 +0200

    Auxiliary find-first functions

--- src/tubax/helpers.cljs
+++ src/tubax/helpers.cljs
@@ -1,12 +1,12 @@
 (ns tubax.helpers)
 
 ;; Datastructure access
-(defn tag [[tag _ _]] tag)
-(defn attributes [[_ attributes _]] attributes)
-(defn children [[_ _ children]] children)
+(defn get-tag [[tag _ _]] tag)
+(defn get-attributes [[_ attributes _]] attributes)
+(defn get-children [[_ _ children]] children)
 
-(defn text [node]
-  (let [[value & _] (children node)]
+(defn get-text [node]
+  (let [[value & _] (get-children node)]
     (if (string? value) value nil)))
 
 ;; Find first
@@ -14,6 +14,52 @@
 ; (find-first result {:path [:rss :channel :description]})
 ; (find-first result {:attribute :isPermaLink})
 ; (find-first result {:attribute [:isPermaLink true]})
+
+(defn search-first [search-fn node]
+  (cond
+    (string? node) nil
+    (search-fn node) node
+    :else
+    (if (empty? (get-children node))
+      nil
+      (some identity (map (partial search-first search-fn)
+                          (get-children node))))))
+
+(defn find-first-by-path [path-left node]
+  (cond
+    (empty? path-left) node
+    (nil? node) nil
+    (string? node) nil
+    :else
+    (let [subtree (some #(if (= (get-tag %) (first path-left)) % nil)
+                        (get-children node))]
+      (recur (rest path-left)
+             subtree))))
+
+(defn find-first [tree {:keys [tag path attribute]}]
+  (cond
+    ;; Searching for tag
+    tag
+    (search-first #(= (get-tag %) tag) tree)
+
+    ;; Searching for path
+    (and path
+         (not (empty? path))
+         (= (first path) (first tree)))
+    (find-first-by-path (rest path) tree)
+
+    ;; Searching for attribute existence
+    (keyword? attribute)
+    (search-first #(contains? (get-attributes %) attribute) tree)
+
+    ;; Searching for attribute equality
+    (and (vector? attribute) (= (count attribute) 2))
+    (search-first #(and (contains? (get-attributes %) (first attribute))
+                        (= (get (get-attributes %) (first attribute)) (second attribute))) tree)
+
+    ;; Not valid
+    :else nil))
+
 
 ;; Find all
 ; (find-all result {:tag :link})
--- test/tubax/helpers_test.cljs
+++ test/tubax/helpers_test.cljs
@@ -6,9 +6,78 @@
 (deftest helpers-access
   (testing "Helpers access"
     (let [node [:item {:att1 "att"} ["value"]]]
-      (is (= (helpers/tag node) :item))
-      (is (= (helpers/attributes node) {:att1 "att"}))
-      (is (= (helpers/children node) ["value"]))
-      (is (= (helpers/text node) "value"))))
+      (is (= (helpers/get-tag node) :item))
+      (is (= (helpers/get-attributes node) {:att1 "att"}))
+      (is (= (helpers/get-children node) ["value"]))
+      (is (= (helpers/get-text node) "value"))))
   (testing "Unexpected values"
-    (is (= (helpers/text [:item {} [[:itemb {} ["value"]]]]) nil))))
+    (is (= (helpers/get-text [:item {} [[:itemb {} ["value"]]]]) nil))))
+
+
+(def testing-data
+  [:rss {:version "2.0"}
+   [[:channel {}
+     [[:title {} ["RSS Title"]]
+      [:description {} ["This is an example of an RSS feed"]]
+      [:link {} ["http://www.example.com/main.html"]]
+      [:lastBuildDate {} ["Mon, 06 Sep 2010 00:01:00 +0000"]]
+      [:pubDate {} ["Sun, 06 Sep 2009 16:20:00 +0000"]]
+      [:ttl {} ["1800"]]
+      [:item {}
+       [[:title {} ["Example entry"]]
+        [:description {} ["Here is some text containing an interesting description."]]
+        [:link {} ["http://www.example.com/blog/post/1"]]
+        [:guid {:isPermaLink "false"} ["7bd204c6-1655-4c27-aaaa-111111111111"]]
+        [:pubDate {:year "2013"} ["Sun, 06 Sep 2013 16:20:00 +0000"]]]]
+      [:item {}
+       [[:title {} ["Example entry2"]]
+        [:description {} ["Here is some text containing an interesting description."]]
+        [:link {} ["http://www.example.com/blog/post/2"]]
+        [:guid {:isPermaLink "true"} ["7bd204c6-1655-4c27-bbbb-222222222222"]]
+        [:pubDate {:year "2009"} ["Sun, 06 Sep 2009 16:20:00 +0000"]]
+        [:author {} "John McCarthy"]]]]]]])
+
+
+(deftest find-first
+  (testing "Find first tag"
+    (is (= (helpers/find-first testing-data {:tag :link})
+           [:link {} ["http://www.example.com/main.html"]]))
+    (is (= (helpers/find-first testing-data {:tag :guid})
+           [:guid {:isPermaLink "false"} ["7bd204c6-1655-4c27-aaaa-111111111111"]]))
+    (is (= (helpers/find-first testing-data {:tag :author})
+           [:author {} "John McCarthy"]))
+    (is (= (helpers/find-first testing-data {:tag :no-tag})
+           nil)))
+  (testing "Find first path"
+    (is (= (helpers/find-first testing-data {:path [:rss :channel :ttl]})
+           [:ttl {} ["1800"]]))
+    (is (= (helpers/find-first testing-data {:path [:rss :channel :item :link]})
+           [:link {} ["http://www.example.com/blog/post/1"]]))
+    (is (= (helpers/find-first testing-data {:path [:rss :channel :item :notexists]})
+           nil))
+    (is (= (helpers/find-first testing-data {:path nil})
+           nil))
+    (is (= (helpers/find-first testing-data {:path []})
+           nil))
+    (is (= (helpers/find-first testing-data {:path [:badroot]})
+           nil)))
+  (testing "Find first keyword"
+    (is (= (helpers/find-first testing-data {:attribute :isPermaLink})
+           [:guid {:isPermaLink "false"} ["7bd204c6-1655-4c27-aaaa-111111111111"]]))
+    (is (= (helpers/find-first testing-data {:attribute :year})
+           [:pubDate {:year "2013"} ["Sun, 06 Sep 2013 16:20:00 +0000"]]))
+    (is (= (helpers/find-first testing-data {:attribute :not-existing})
+           nil)))
+  (testing "Find first keyword equality"
+    (is (= (helpers/find-first testing-data {:attribute [:isPermaLink "false"]})
+           [:guid {:isPermaLink "false"} ["7bd204c6-1655-4c27-aaaa-111111111111"]]))
+    (is (= (helpers/find-first testing-data {:attribute [:isPermaLink "true"]})
+           [:guid {:isPermaLink "true"} ["7bd204c6-1655-4c27-bbbb-222222222222"]]))
+    (is (= (helpers/find-first testing-data {:attribute [:year "2013"]})
+           [:pubDate {:year "2013"} ["Sun, 06 Sep 2013 16:20:00 +0000"]]))
+    (is (= (helpers/find-first testing-data {:attribute [:year "2009"]})
+           [:pubDate {:year "2009"} ["Sun, 06 Sep 2009 16:20:00 +0000"]]))
+    (is (= (helpers/find-first testing-data {:attribute [:year "2010"]})
+           nil))
+    (is (= (helpers/find-first testing-data {:attribute [:not-existing true]})
+           nil))))
