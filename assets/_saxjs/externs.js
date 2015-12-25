9b5e0ef1e99bbc2749fa9a9a4359b320b9f68dcd
Andrey Antukh <niwi@niwi.nz>
2015-12-25 13:41:37 +0200

    Add proper hand made externs file.

--- assets/_saxjs/externs.js
+++ assets/_saxjs/externs.js
@@ -1,14 +1,95 @@
-var TopLevel = {
-"attributes" : function () {},
-"close" : function () {},
-"Error" : function () {},
-"name" : function () {},
-"onclosetag" : function () {},
-"onend" : function () {},
-"onerror" : function () {},
-"onopentag" : function () {},
-"ontext" : function () {},
-"parser" : function () {},
-"sax" : function () {},
-"write" : function () {}
-}
+/**
+ * @const
+ */
+var sax = function() {};
+
+/**
+ * @constructor
+ * @dict
+ */
+sax._Node = function() {};
+
+/**
+ * @constructor
+ */
+sax.SAXParser = function() {};
+
+/**
+ * @constructor
+ */
+sax.SAXStream = function() {};
+
+/**
+ * @this {null}
+ * @return {sax.SAXStream}
+ */
+sax.createStream = function() {};
+
+/**
+ * @this {null}
+ * @return {sax.SAXParser}
+ */
+sax.parser = function() {};
+
+/**
+ * @this {sax.SAXParser}
+ * @return {null}
+ */
+sax.SAXParser.prototype.write = function() {};
+
+/**
+ * @this {sax.SAXParser}
+ * @return {null}
+ */
+sax.SAXParser.prototype.close = function() {};
+
+/**
+ * @this {sax.SAXParser}
+ * @return {null}
+ */
+sax.SAXParser.prototype.flush = function() {};
+
+/**
+ * @this {sax.SAXParser}
+ * @return {null}
+ */
+sax.SAXParser.prototype.resume = function() {};
+
+/**
+ * @this {sax.SAXParser}
+ * @return {null}
+ */
+sax.SAXParser.prototype.end = function() {};
+
+/**
+ * @this {sax.SAXParser}
+ */
+sax.SAXParser.prototype.onopentag = function() {};
+
+/**
+ * @this {sax.SAXParser}
+ * @param {sax._Node}
+ * @type {function()}
+ */
+sax.SAXParser.prototype.onclosetag = function() {};
+
+/**
+ * @this {sax.SAXParser}
+ * @param {sax._Node}
+ * @type {function()}
+ */
+sax.SAXParser.prototype.ontext = function() {};
+
+/**
+ * @this {sax.SAXParser}
+ * @param {sax._Node}
+ * @type {function()}
+ */
+sax.SAXParser.prototype.onend = function() {};
+
+/**
+ * @this {sax.SAXParser}
+ * @param {sax._Node}
+ * @type {function()}
+ */
+sax.SAXParser.prototype.onerror = function() {};
